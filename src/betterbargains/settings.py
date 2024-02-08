29113f384f75e39b33af4da9780551b7ce9eb522
noahwekesa <noahwekesa0@gmail.com>
2024-02-08 23:52:06 +0300

    changes

--- src/betterbargains/settings.py
+++ src/betterbargains/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(66v8hxejgkp9@o!wt^r5xtyb%tem+3r1l91@7ey^c&efmg&ps
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 SESSION_COOKIE_AGE = 86400
 CART_SESSION_ID = 'cart'
