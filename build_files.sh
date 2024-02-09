31bae88d61f1c898d9625f574a5d5923a5ad2374
noahwekesa <noahwekesa0@gmail.com>
2024-02-09 22:14:58 +0300

    script

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#
-# build_files.sh
-pip install -r requirements.txt -y
-pip install django-dotenv
-cd "$(dirname "$0")"
+python3 -m venv venv
 
-cd "src"
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+cd src
 
 python3.9 manage.py collectstatic
