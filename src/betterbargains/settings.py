589cdf8fde29d0a22831c934cb7039a610c8f08d
noahwekesa <noahwekesa0@gmail.com>
2024-02-13 03:26:09 +0300

    is

--- src/betterbargains/settings.py
+++ src/betterbargains/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "betterbargains", "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIRS = [BASE_DIR, "static"]
