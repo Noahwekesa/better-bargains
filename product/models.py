cf6ead65bd0343325f5b5719d19ef295b6818015
noahwekesa <noahwekesa0@gmail.com>
2024-02-15 16:16:50 +0300

    max length

--- product/models.py
+++ product/models.py
@@ -5,29 +5,29 @@ from io import BytesIO
 from PIL import Image
 from django_extensions.db.fields import AutoSlugField
 
+
 class Category(models.Model):
     name = models.CharField(max_length=24, choices=CATEGORY)
     slug = models.SlugField()
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
 
 class Product(models.Model):
-    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
     category = models.ForeignKey(
-        Category,
-        related_name='products',
-        on_delete=models.CASCADE)
-    name = models.CharField(max_length=24)
-    slug = AutoSlugField(populate_from='name')
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=150)
+    slug = AutoSlugField(populate_from="name")
     description = models.TextField(blank=True, null=True)
     price = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="uploads/", blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     def get_display_price(self):
         return self.price
@@ -42,15 +42,15 @@ class Product(models.Model):
 
                 return self.thumbnail.url
             else:
-                return 'https://via.placeholder.com/240x240x.jpg'
+                return "https://via.placeholder.com/240x240x.jpg"
 
     def make_thumbnail(self, image, size=(300, 300)):
         img = Image.open(image)
-        img.convert('RGB')
+        img.convert("RGB")
         img.thumbnail(size)
 
         thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
+        img.save(thumb_io, "JPEG", quality=85)
 
         thumbnail = File(thumb_io, name=image.name)
 
