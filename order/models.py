156cf71f99e8c65ab6a42b9f5d181347d8a8a94b
noahwekesa <noahwekesa0@gmail.com>
2024-02-25 02:03:50 +0300

    order models

--- order/models.py
+++ order/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from product.models import Product
+
 
 class Order(models.Model):
+
+    ORDERED = "ordered"
+    SHIPPED = "shipped"
+
+    STATUS_CHOICES = ((ORDERED, "ordered"), (SHIPPED, "shipped"))
+
     user = models.ForeignKey(
         User, related_name="orders", blank=True, null=True, on_delete=models.CASCADE
     )
@@ -11,10 +19,19 @@ class Order(models.Model):
     email = models.EmailField()
     phone = models.PositiveIntegerField()
     location = models.CharField(max_length=255)
-    place = models.CharField(max_lenght=255)
+    place = models.CharField(max_length=255)
     created_at = models.DateTimeField(
         auto_now_add=True
     )  # check when the order was created
 
     paid = models.BooleanField(default=False)  # check the status of the order
-    paid_amount = (models.IntegerField(blank=True, null=True),)  # Amount to be paid
+    paid_amount = models.IntegerField(blank=True, null=True)  # Amount to be paid
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=ORDERED)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="items", on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=1)
