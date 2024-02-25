c346873289f3a5819f8b8219424f6b6c33134645
noahwekesa <noahwekesa0@gmail.com>
2024-02-25 15:15:11 +0300

    product

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name = "polls"
+app_name = "product"
 
 urlpatterns = [
     path("product_detail/<slug:slug>", views.product_detail, name="product"),
