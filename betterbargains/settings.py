97b1c0d387d1e399b2f4cbb188c7bd3b65dd7833
noahwekesa <noahwekesa0@gmail.com>
2024-03-07 01:11:07 +0300

    changes

--- betterbargains/settings.py
+++ betterbargains/settings.py
@@ -99,24 +99,24 @@ WSGI_APPLICATION = "betterbargains.wsgi.app"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("PGDATABASE"),
-#         "USER": os.getenv("PGUSER"),
-#         "PASSWORD": os.getenv("PGPASSWORD"),
-#         "HOST": os.getenv("PGHOST", 5432),
-#         "PORT": "5432",
-#         "OPTIONS": {"sslmode": "require"},
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PGDATABASE"),
+        "USER": os.getenv("PGUSER"),
+        "PASSWORD": os.getenv("PGPASSWORD"),
+        "HOST": os.getenv("PGHOST", 5432),
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
 
 
 # Password validation
